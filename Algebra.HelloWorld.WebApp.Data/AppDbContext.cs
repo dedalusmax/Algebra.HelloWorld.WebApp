5bdc93286f14f55e5c7418c885674a8b926e67ef
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-11-28 20:18:01 +0100

    ctrl

--- Algebra.HelloWorld.WebApp.Data/AppDbContext.cs
+++ Algebra.HelloWorld.WebApp.Data/AppDbContext.cs
@@ -5,6 +5,11 @@ namespace Algebra.HelloWorld.WebApp.Data;
 
 public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
     public DbSet<Movie> Movies { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
